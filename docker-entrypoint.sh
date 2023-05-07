6fb7edfb6e24038786f3949f013bbe8b5698aa2e
blinxen <h-k-81@hotmail.com>
2023-05-07 18:19:00 +0200

    [fleet-manager] Make entrypoint generic

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,7 +2,5 @@
 
 source /opt/ros/humble/setup.bash
 source /usr/src/app/install/setup.bash
-source /rmf_demos_ws/install/setup.bash
 
 exec zenoh-bridge-dds --config /etc/zenoh-bridge-dds/config.json5 &
-exec ros2 launch free_fleet_rmf_adapter adapter.launch.xml
