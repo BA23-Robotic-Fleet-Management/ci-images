ad8d43321acecf557837c99cb4e52cc36e5594f9
blinxen <h-k-81@hotmail.com>
2023-06-04 20:22:58 +0200

    [fleet-manager] Reintroduce zenoh to base image

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Source ROS2 env
 source /opt/ros/humble/setup.bash
 source /usr/src/app/install/setup.bash
 
+# Start zenoh bridge
+exec zenoh-bridge-dds --config /etc/zenoh-bridge-dds/config.json5 &
